4277f17fa431d380e44083fdc4aee35676d22cec
David Prause <praused@hotmail.com>
2017-10-27 11:36:51 -0400

    Add project files.

---
+++ Sixeyed.ExtensionLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sixeyed.ExtensionLibrary
+{
+    public class Class1
+    {
+    }
+}
