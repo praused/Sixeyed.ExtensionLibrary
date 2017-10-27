6c4a66b44c60076bae5a5a86da93b53e0aef6b0e
David Prause <praused@hotmail.com>
2017-10-27 16:01:25 -0400

    Segment 1, Demo 1, Exceptions and Enums, Part 2

--- Sixeyed.ExtensionLibrary.Tests/Demo1/EnumExtensionsTests.cs
+++ Sixeyed.ExtensionLibrary.Tests/Demo1/EnumExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using componentModel = System.ComponentModel;
 
 namespace Sixeyed.ExtensionLibrary.Tests.Demo1
 {
@@ -7,8 +8,39 @@ namespace Sixeyed.ExtensionLibrary.Tests.Demo1
     public class EnumExtensionsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetName()
         {
+            var intro = Module.Intro;
+            Assert.AreEqual("Intro", intro.ToString());
+            Assert.AreEqual("Intro", Enum.GetName(typeof(Module), intro));
+            Assert.AreEqual("Intro", intro.GetName());
+        }
+
+        [TestMethod]
+        public void GetDescription()
+        {
+            Assert.AreEqual("Introducing Extension Methods", Module.Intro.GetDescription());
+            Assert.AreEqual("Advanced", Module.Advanced.GetDescription());
+
+            Assert.AreEqual("In Progress", ModuleStatus.InProgress.GetDescription());
+            Assert.AreEqual("Done", ModuleStatus.Done.GetDescription());
+        }
+
+        public enum Module
+        {
+            [componentModel.Description("Introducing Extension Methods")]
+            Intro,
+            Advanced,
+            Library
+
+        }
+
+        public enum ModuleStatus
+        {
+            ToDo,
+            [componentModel.Description("In Progress")]
+            InProgress,
+            Done
         }
     }
 }
