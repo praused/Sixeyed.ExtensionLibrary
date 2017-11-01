8b638e4abdf9b34db1b107d887b6ead39a3899e5
David Prause <praused@hotmail.com>
2017-11-01 11:33:31 -0400

    Segment 4, Demo 3, Entity Framework, WIP

---
+++ Sixeyed.ExtensionLibrary.Tests/Demo3/DomainModelTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sixeyed.ExtensionLibrary.Domain.Model;
+
+namespace Sixeyed.Extensions.Samples.Tests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void SaveChanges()
+        {
+            ////New version of EF does not have DomainModelContainer
+            //using (var container = new DomainModelContainer())
+            //{
+
+            //}
+        }
+    }
+}
